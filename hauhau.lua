543030a5c3745ca1ca8aff28e2ed4108b998b18c
hauhau90 <tborcescu@gmail.com>
2016-08-10 18:20:01 +0300

    Add files via upload

---
+++ hauhau.lua
@@ -0,0 +1,16 @@
+local version = "0.1 Alpha";
+local author = "hauhau";
+local SCRIPT_NAME = "hauhau's elo grinder"
+local AUTOUPDATE = true;
+local UPDATE_HOST = "raw.githubusercontent.com"
+local UPDATE_PATH = "/hauhau90/
+
+function SayHello()
+	-- Print name and version
+		PrintChat("Welcome to hauhau's elo grinder v.01 alpha!")
+		end
+		
+function OnLoad()
+				SayHello()
+end
+
